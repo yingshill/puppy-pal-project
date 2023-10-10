510ce0b220b39fa827f8800227260351de3e56a1
Yingshi Liu <serendipper.liu@gmail.com>
2023-10-10 12:43:12 -0700

    render puppylist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
-import './App.css'
+/* import './App.css' */
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ /*  const [count, setCount] = useState(0) */
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
-    <>
-    </>
+    <div>
+        { puppies.map((puppy) => {return <p key={puppy.id}>{puppy.name}</p>}) }
+    </div>
   )
 }
 
